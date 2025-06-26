8e03e23aee6a9712e10ee1b902d00dc2cdda7677
Nadimpalli Hema nagamani <hemanadimpalli6@gmail.com>
2025-06-26 12:08:09 +0530

    shopping react ui

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainPage from './shoppingfolder/pages/MainPage'
+import './App.css'
+const App = () => {
+  return (
+    <div>
+      <MainPage />
+    </div>
+  )
+}
+
+export default App
---
+++ src/shoppingfolder/components/Footer.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer-section'>
+        <div className="banner-box">
+        <img src="assets/footer.png" alt="footer" />
+      </div>
+    </div>
+  )
+}
+
+export default Footer
